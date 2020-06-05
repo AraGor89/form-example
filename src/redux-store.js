4724a38c5d2abeda8038476173968cdcd2f11603
GorAraqelyan <50023373+GorAraqelyan@users.noreply.github.com>
2020-06-05 10:44:02 +0400

    init

--- src/redux-store.js
+++ src/redux-store.js
@@ -1,11 +1,15 @@
-import { combineReducers, createStore, compose } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
 import { reducer as formReducer } from "redux-form";
+import thunkMiddleware from "redux-thunk";
 
 let reducers = combineReducers({
   form: formReducer,
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(reducers, composeEnhancers());
+let store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunkMiddleware))
+);
 
 export default store;
